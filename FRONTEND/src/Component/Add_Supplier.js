dbc487a5daad9f2f0464f14532ed726849c444a5
CJeew <128817833+CJeew@users.noreply.github.com>
2024-04-05 00:37:45 +0530

    added new variables into supplier function and modified the frontend of supplier function

--- FRONTEND/src/Component/Add_Supplier.js
+++ FRONTEND/src/Component/Add_Supplier.js
@@ -6,8 +6,13 @@ import axios from "axios";
 function Add_Supplier() {
     const [name, setName] = useState("");
     const [contact, setContact] = useState("");
+    const [contact_2, setContact_2] = useState("");
+    const [Email,setEmail] =useState("");
+    const [Website,setWeb] =useState("");
+    const [Main_supplies,setMainSupplies] = useState("");
+    const [Additional_note,setAdditional_notes] =useState("");
     const [address, setAddress] = useState(""); // Include the 'address' state
-    const [country, setCountry] = useState("");
+    // const [country, setCountry] = useState("");
 
     function submit(e) {
         e.preventDefault();
@@ -15,8 +20,13 @@ function Add_Supplier() {
         const newSupplier = {
             name,
             contact,
+            contact_2,
+            Email,
+            Website,
+            Main_supplies,
+            Additional_note,
             address, // Include 'address' in the newSupplier object
-            country
+           
         };
 
         axios.post("http://localhost:8090/supplier/add", newSupplier)
@@ -32,32 +42,83 @@ function Add_Supplier() {
         <div>
         <div className="container bg-gray-200 bg-opacity-70 rounded-lg px-8 py-4 mt-3 mx-5">
             <h1 className="Heading1 text-center text-3xl flex-grow font-BakBak one font-bold">Add Supplier</h1>
-            <div className="flex justify-between items-center">
+            
                 <form className="form1 text-lg text-gray-950 ">
-                <div className="flex flex-wrap justify-between items-center space-x-2 py-2">
-                    <label>Supplier Name</label>
+
+                <div className="space-y-2 flex justify-between grid grid-cols-2 gap-4 ">
+                <div>
+                    <label>Supplier Name    :</label>
                     <input type="text" onChange={(e) => setName(e.target.value)} />
                 </div>
-                <div className="flex flex-wrap justify-between items-center space-x-5 py-2">
 
-                    <label>Contact</label>
+                <div>
+                    <label>Contact  :</label>
                     <input type="text" onChange={(e) => setContact(e.target.value)} />
+                </div>
+                </div>   
+                 
+                <div className="space-y-2 flex justify-between grid grid-cols-2 gap-4 ">   
+                    <div>
+                    <label>Contact 2    :</label>
+                    <input type="text" onChange={(e) => setContact_2(e.target.value)} />
                     </div>
 
-                    <div className="flex flex-wrap justify-between items-center space-x-2 py-2">
+                    <div>
+                    <label>E mail   :</label>
+                    <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                </div>
 
-                    <label>Address</label>
+
+                
+                <div className="space-y-2 flex justify-between grid grid-cols-2 gap-4 ">
+                    <div>
+                    <label>Address  :</label>
                     <input type="text" onChange={(e) => setAddress(e.target.value)} />
                     </div>
+                    <div>
+                    <label>Web Site :</label>
+                    <input type="text" onChange={(e) => setWeb(e.target.value)} />
+                    </div>
+                        
+                    
+                    </div>
                     <div className="flex flex-wrap justify-between items-center space-x-2 py-2">
+                        <div className="flex flex-col">
+                    <label>Additional Notes :</label>
+                    <textarea
+                     className="mt-1 rounded-md border border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    rows="4"
+                    cols="50"
+                     onChange={(e) => setAdditional_notes(e.target.value)} />
+                    </div>
+                    <div className="flex flex-col">
+                            <label>Main Supplies    :</label>
+                            <textarea 
+                                className="mt-1 rounded-md border border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                rows="4"
+                                cols="50"
+                                onChange={(e) => setMainSupplies(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    
+                    
+                    {/* <div className="flex flex-wrap justify-between items-center space-x-2 py-2">
 
                     <label>Country</label>
                     <input type="text" onChange={(e) => setCountry(e.target.value)} />
-                    </div>
-                    <input className="bg-red-800 radius-2rm " type="submit" value={"Add Supplier"} onClick={submit} /> 
+                    </div> */}
+                    {/* <input className="bg-red-800 radius-2rm " type="submit" value={"Add Supplier"} onClick={submit} />  */}
+                   <div className="flex justify-center">
+                    <button className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
+                                     hover:to-amber-700 text-white font-bold py-4 px-5 rounded-lg mr-2 opacity-90 transition duration-300 
+                                     ease-in-out transform hover:scale-105" type="submit" onClick={submit} >Add Supplier</button>
+                                     </div>
+
                 </form>
 
-            </div>
+            
 
         </div>
         
