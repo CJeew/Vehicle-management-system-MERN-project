3f1a26a9f38496815e09c2a77745fe6cd9bae556
Your Name <you@example.com>
2024-04-10 11:54:55 +0530

    shajeeh marge

--- FRONTEND/src/Component/Package_edithome.js
+++ FRONTEND/src/Component/Package_edithome.js
@@ -2,12 +2,9 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
-<<<<<<< HEAD
-=======
 import 'primeicons/primeicons.css';
         
 
->>>>>>> a387a87c877e25e6bc91b446bc80b2bc0e6344d5
 
 export default function Package_edithome() {
   const [packages, setPackages] = useState([]);
@@ -27,37 +24,6 @@ export default function Package_edithome() {
   }, []);
   return (
     <div>
-<<<<<<< HEAD
-       <div className="text-2xl flex justify-center items-center">
-    <div className="gap-16 grid grid-cols-2 mt-8">
-      <a href="viewpkg">
-        <button className="bg-gradient-to-r from-red-400 to-red-500 px-2 py-4 text-white font-bold uppercase hover:bg-red-600 hover:text-black rounded-[10px] w-full ">
-        Interior
-        </button>
-        </a>
-        <a href="viewpkg2">
-        <button className="bg-gradient-to-r from-red-400 to-red-500 opacity-70 px-2 py-4 text-white font-bold uppercase hover:bg-red-600 hover:text-black rounded-[10px] w-full">
-        Exterior
-        </button>
-        </a>
-        </div>
-        </div>
-      <div className="flex justify-center items-center w-full">
-        <div className="grid grid-cols-2 gap-8 bg-gray-200 bg-opacity-70 rounded-lg p-14 mt-16 mx-5 max-w-7xl">
-
-          {packages.filter((pkg) => pkg.category === "Interior").map((pkg) => (
-            <div
-              className="flex flex-col justify-center items-center bg-black bg-opacity-75 text-white rounded-lg px-8 py-4"
-              key={pkg.pid}
-            >
-              <h2 className="text-xl font-bold mb-2">{pkg.name}</h2>
-              <p className="text-base text-gray-200">{pkg.description}</p>
-              <h4 className="text-lg text-red-700 font-bold">RS. {pkg.unitprice.toFixed(2)}</h4>
-              <button className="bg-red-500 text-white font-bold py-2 px-4 rounded mt-4">
-                </button>
-            </div>
-          ))}
-=======
         <h2 className="text-3xl mb-6 flex justify-center mt-8 font-bold text-white"> Interior</h2>
       <div className="flex justify-center flex-col items-center w-full  ">
         <div className="grid grid-cols-2 gap-8 bg-gray-200 bg-opacity-70 rounded-lg p-14 mt-16 mx-5 w-full max-w-7xl">
@@ -109,7 +75,6 @@ export default function Package_edithome() {
                 </a>
               </div>
             ))}
->>>>>>> a387a87c877e25e6bc91b446bc80b2bc0e6344d5
         </div>
       </div>
     </div>
