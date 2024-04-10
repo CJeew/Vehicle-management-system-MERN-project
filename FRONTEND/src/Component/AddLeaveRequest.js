046be18a6a9e5fc58276ce8868838b18c5099853
SanjayanT <majorsanjayan@gmail.com>
2024-04-10 16:23:50 +0530

    sanjayan updated

--- FRONTEND/src/Component/AddLeaveRequest.js
+++ FRONTEND/src/Component/AddLeaveRequest.js
@@ -55,6 +55,7 @@ export default function AddLeaveRequest() {
                   name="nic"
                   id="nic"
                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  pattern="[0-9]{9}[vV]{1}|[0-9]{12}"
                   onChange={(e)=>{
 
                     setNic(e.target.value);
