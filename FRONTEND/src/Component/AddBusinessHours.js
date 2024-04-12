ef2426b15eec32cfe1e91857b1e869c60e560d53
Tavini2002 <taviniperera2002@gmail.com>
2024-04-12 10:09:46 +0530

    managerlogin

--- FRONTEND/src/Component/AddBusinessHours.js
+++ FRONTEND/src/Component/AddBusinessHours.js
@@ -48,15 +48,15 @@ function AddBusinessHours(){
                     <div class="justify-center form-group flex mx-10">
                         <label for="day" class="block text-sm font-medium text-black my-4 mt-10">Day of The Week : </label>
                         <select id="vehicle-type" name="vehicle-type" class="mr-5 ml-5 h-10 mt-7 p-2 block border-gray-300 rounded-md text-black" onChange={(e) => setDay(e.target.value)}>
-                                        <option value="">Select a Day</option> {/* Add an empty option for default selection */}
-                                        <option value="Monday">Monday</option>
-                                        <option value="Tuesday">Tuesday</option>
-                                        <option value="Wednesday">Wednesday</option>
-                                        <option value="Thursday">Thursday</option>
-                                        <option value="Friday">Friday</option>
-                                        <option value="Saturday">Saturday</option>
-                                        <option value="Sunday">Sunday</option>
-                                    </select>    
+                                <option value="">Select a Day</option> {/* Add an empty option for default selection */}
+                                <option value="Monday">Monday</option>
+                                <option value="Tuesday">Tuesday</option>
+                                <option value="Wednesday">Wednesday</option>
+                                <option value="Thursday">Thursday</option>
+                                <option value="Friday">Friday</option>
+                                <option value="Saturday">Saturday</option>
+                                <option value="Sunday">Sunday</option>
+                        </select>    
                     </div> 
 
                     
--- FRONTEND/src/Component/BookingPageManagerLogin.js
+++ FRONTEND/src/Component/BookingPageManagerLogin.js
@@ -2,18 +2,27 @@ import React from "react";
 import "./Home.css";
 import "./BookingPage.js";
 import "./BookingPage.css";
-import imgSrc3 from "./vacuum.jpg";
+//import imgSrc3 from "./vacuum.jpg";
 
 export default function BookingPageManagerLogin() {
   return (
     <div>
       <div className="relative">
         <h1 className="ms-20 my-10 mt-20 m text-6xl font-extrabold text-white">Book Now</h1>
-        <img src={imgSrc3} alt="Vacuum Image" className="absolute top-4 right-10 mx-60 rounded-2xl" style={{ width: "400px", height: "600px" }}/>
-            <ul>
-                <li className="ms-20  text-2xl font-semibold text-gray-400 dark:text-gray-400 hover:text-red-600">Booking List</li>
-                <li className="ms-20 my-10 text-2xl font-semibold text-gray-400 dark:text-gray-400 hover:text-red-600">Appointment Calendar</li>
-                <li className="ms-20 my-10 text-2xl font-semibold text-gray-400 dark:text-gray-400 hover:text-red-600">Staff Shifts</li>
+            <ul class="flex mx-10 mb-10 ">
+                <li class="mx-20 mt-10"><a href="/bookRead" class="ms-10 me-5 block max-w-sm p-6 bg-black border 
+                border-black-200 rounded-lg shadow-md hover:bg-black-100 dark:bg-black-800 dark:border-black-700 
+                dark:hover:bg-black-700 opacity-80 transition duration-300 ease-in-out transform hover:scale-105">
+
+                <h5 class="mb-2 text-3xl font-bold tracking-tight text-white">Booking List</h5><br></br>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Review the booking list, complete with detailed information, ensuring comprehensive oversight and management of all reservations.</p></a></li>
+                
+                <li class="mx-10 mt-10"><a href="#" class="ms-10 me-5 block max-w-sm p-6 bg-black border 
+                border-black-200 rounded-lg shadow-md hover:bg-black-100 dark:bg-black-800 dark:border-black-700 
+                dark:hover:bg-black-700 opacity-80 transition duration-300 ease-in-out transform hover:scale-105">
+
+                <h5 class="mb-2 text-3xl font-bold tracking-tight text-white">Business Hour Settings</h5><br></br>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Review the booking list, complete with detailed information, ensuring comprehensive oversight and management of all reservations.</p></a></li>
             </ul>
         </div>
       </div>
