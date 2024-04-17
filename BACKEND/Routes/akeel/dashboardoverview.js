a652f732026f436d4806c37abd98db24dd91a5ec
akeel2000 <noormohommaduakeel@gmail.com>
2024-04-17 09:43:56 +0530

    akeelnew

--- BACKEND/Routes/akeel/dashboardoverview.js
+++ BACKEND/Routes/akeel/dashboardoverview.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const ManageParts = require("../../Models/manageparts.js");
 const IssuedItems = require("../../Models/issueditems.js");
-const ManageOrders = require("../../Models/manageorders.js")
+const ManageOrders = require("../../Models/manageorders.js");
+
 
 // Endpoint to fetch dashboard overview data
 router.get('/overview', async (req, res) => {
@@ -10,6 +11,7 @@ router.get('/overview', async (req, res) => {
       const totalParts = await ManageParts.countDocuments();
       const totalIssuedItems = await IssuedItems.countDocuments();
       const totalOrders = await ManageOrders.countDocuments();
+      // const totatvalues = await ManageParts.calculateDocuments();
       
       // Calculate other metrics as needed
       
@@ -18,6 +20,7 @@ router.get('/overview', async (req, res) => {
         totalParts,
         totalIssuedItems,
         totalOrders,
+        // totalvalues,
         // Add more metrics here
       });
     } catch (error) {
@@ -25,5 +28,6 @@ router.get('/overview', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
 
 module.exports = router;
