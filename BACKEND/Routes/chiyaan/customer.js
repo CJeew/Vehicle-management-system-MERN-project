e1e834bae6726ecfad6cadd92952e026003d71d9
Sivashangar007 <chiyaan0825@gmail.com>
2024-04-29 13:37:50 +0530

    chiyaan29/04/2024

--- BACKEND/Routes/chiyaan/customer.js
+++ BACKEND/Routes/chiyaan/customer.js
@@ -1,102 +1,116 @@
 const router = require("express").Router();
 let customer = require("../../Models/customer");
 
-
 //create - working
 
-router.route("/Register").post(async(req,res)=>{
+router.route("/Register").post(async (req, res) => {
+  const cname = req.body.cname;
+  const cnic = req.body.cnic;
+  const cpass = req.body.cpass;
+  const cpass2 = req.body.cpass2;
+  const cphone = req.body.cphone;
+  const cmail = req.body.cmail;
+  const cvnum = req.body.cvnum;
+  const cvtype = req.body.cvtype;
+  const exisitingEmail = await customer.findOne({ cmail });
+  if (exisitingEmail) {
+    return res.status(404).json({ message: "The Email alrady exsist" });
+  }
+ 
+  const newCustomer = new customer({
+    cname,
+    cnic,
+    cpass,
+    cpass2,
+    cphone,
+    cmail,
+    cvnum,
+    cvtype,
+  });
 
-    const cname = req.body.cname;
-    const cnic = req.body.cnic;
-    const cpass = req.body.cpass;
-    const cpass2 = req.body.cpass2; 
-    const cphone = req.body.cphone;
-    const cmail = req.body.cmail;
-    const cvnum = req.body.cvnum;
-    const cvtype = req.body.cvtype;
-    const exisitingEmail = await customer.findOne({cmail});
-    if(exisitingEmail){
-        return res.status(404).json({message : 'The Email alrady exsist'});
-    }
-    const newCustomer = new customer({
-        cname,
-        cnic,
-        cpass,
-        cpass2,
-        cphone,
-        cmail,
-        cvnum,
-        cvtype
+  newCustomer
+    .save()
+    .then(() => {
+      res.json("Registeration Done");
     })
-
-    newCustomer.save().then(()=>{
-        res.json("Registeration Done");
-    }).catch((err)=>{
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 });
 
 //read - working
 
-router.route("/").get((req,res)=>{
-    customer.find().then((customer)=>{
-        res.json(customer);
-    }).catch((err)=>{
-        console.log(err);
+router.route("/").get((req, res) => {
+  customer
+    .find()
+    .then((customer) => {
+      res.json(customer);
     })
-})
-
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //update
 
-router.route("/updatecustomer/:id").post(async(req,res)=>{
-    let cusid = req.params.id;
-    const{cname, cnic, cphone, cmail, cpass, cpass2,cvnum, cvtype, } = req.body; 
+router.route("/Updatecustomer/:id").post(async (req, res) => {
+  let cusid = req.params.id;
+  const { cname, cnic, cphone, cmail, cpass, cpass2, cvnum, cvtype } = req.body;
 
-    const updatecustomer = {
-        cname,
-        cnic,
-        cphone,
-        cmail,
-        cpass,
-        cpass2,
-        cvnum,
-        cvtype,
-        
-    }
-    const update = await customer.findByIdUpdate(cusid, updatecustomer)  //await - waiting until the before update finish to execute next update
-    .then(()=>{  
-        res.status(200).send({status: "Details Updated"})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating data", error: err.message});
+  const Updatecustomer = {
+    cname,
+    cnic,
+    cphone,
+    cmail,
+    cpass,
+    cpass2,
+    cvnum,
+    cvtype,
+  };
+  const update = await customer
+    .findByIdUpdate(cusid, Updatecustomer) //await - waiting until the before update finish to execute next update
+    .then(() => {
+      res.status(200).send({ status: "Details Updated" });
     })
- })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+});
 
- //delete
+//delete
 
-
- router.route("/delete/:id").delete(async(req, res)=>{
-    let cusid = req.params.id;
-    await customer.findByIdAndDelete(cusid)
-    .then(()=> {
-        res.status(200).send({status: "Profile deleted"});
-    }).catch((err)=>{
-        console.log(err.message);
-        res.status(500).send({stats: "Error with deleting Profile", error:err.message});
+router.route("/delete/:id").delete(async (req, res) => {
+  let cusid = req.params.id;
+  await customer
+    .findByIdAndDelete(cusid)
+    .then(() => {
+      res.status(200).send({ status: "Profile deleted" });
     })
- })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ stats: "Error with deleting Profile", error: err.message });
+    });
+});
 
- //fetch data of one booking
- router.route("/cget/:id").get(async(req,res)=>{
-    let cusid = req.params.id;
-    const customer = await customer.findById(cusid)  //primary key - .findOne(email)
-    .then(()=>{
-        res.status(200).send({status: "customer Fetched", customer: customer})
-    }).catch(()=>{
-        console.log(err.message);
-        res.status(500).send({status: "Error with get profile", error: err.message});
+//fetch data of one booking
+router.route("/get/:id").get(async (req, res) => {
+  let cusid = req.params.id;
+  const customer = await customer
+    .findById(cusid) //primary key - .findOne(email)
+    .then(() => {
+      res.status(200).send({ status: "customer Fetched", customer: customer });
     })
- })
+    .catch(() => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with get profile", error: err.message });
+    });
+});
 
 module.exports = router;
--- FRONTEND/src/Component/Chome.js
+++ FRONTEND/src/Component/Chome.js
@@ -7,42 +7,63 @@ import imgSrc from "./789.png";
 import imgSrc1 from "./741.jpg";
 import imgSrc2 from "./uuu.jpg";
 
-
 export default function Chome() {
   return (
     <div>
       <div className="relative">
-        <h1 className="ms-20 mt-20 m text-6xl font-extrabold text-white">Customer Login</h1>
-        <img src={imgSrc2} alt="Wipe Image" className="absolute top-4 right-10 mx-60 rounded-2xl" style={{ width: "400px", height: "500px" }}/>
+        <h1 className="ms-20 mt-20 m text-6xl font-extrabold text-white">
+          Customer Login
+        </h1>
+        <img
+          src={imgSrc2}
+          alt="Wipe Image"
+          className="absolute top-4 right-10 mx-60 rounded-2xl"
+          style={{ width: "400px", height: "500px" }}
+        />
       </div>
       <br />
-      <small className="ms-20 text-xl opacity-90 mt-5 text-white">Profile button provides Manage your personal details,  </small>
+      <small className="ms-20 text-xl opacity-90 mt-5 text-white">
+        Profile button provides Manage your personal details,{" "}
+      </small>
       <br />
-      <small className="ms-20 text-xl opacity-90 mt-5 text-white"> including editing or deleting your information  </small>
+      <small className="ms-20 text-xl opacity-90 mt-5 text-white">
+        {" "}
+        including editing or deleting your information{" "}
+      </small>
       <br />
-      <small className="ms-20 text-xl opacity-90 mt-5 text-white">Review button provides Share your thoughts and experiences about </small>
+      <small className="ms-20 text-xl opacity-90 mt-5 text-white">
+        Review button provides Share your thoughts and experiences about{" "}
+      </small>
       <br />
-      <small className="ms-20 text-xl opacity-90 mt-5 text-white"> our services or products with others</small>
+      <small className="ms-20 text-xl opacity-90 mt-5 text-white">
+        {" "}
+        our services or products with others
+      </small>
       <br />
-      
-      
-     
 
       <div className="flex ms-20 my-20">
         <figure>
           <img className="rounded-full w-40 h-40 ms-20" src={imgSrc} />
           <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400 ms-20">
-            <button className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
+            <button
+              className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
                                   hover:to-amber-700 text-white font-bold py-4 px-6 rounded-lg mr-2 opacity-90 transition duration-300
-                                  ease-in-out transform hover:scale-105"><a href="/Profile">Profile</a></button>
+                                  ease-in-out transform hover:scale-105"
+            >
+              <a href="/Profile">Profile</a>
+            </button>
           </figcaption>
         </figure>
         <figure>
           <img className="rounded-full w-40 h-40 ms-20" src={imgSrc1} />
           <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400 ms-20">
-            <button className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
+            <button
+              className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
                                   hover:to-amber-700 text-white font-bold py-4 px-6 rounded-lg mr-2 opacity-90 transition duration-300
-                                  ease-in-out transform hover:scale-105"><a href="/Review">Review</a></button>
+                                  ease-in-out transform hover:scale-105"
+            >
+              <a href="/Review">Review</a>
+            </button>
           </figcaption>
         </figure>
       </div>
@@ -50,7 +71,7 @@ export default function Chome() {
     // <div class="h-screen w-screen bg-gray flex justify-center items-center">
     //   <div class="relative inline-flex  group">
     //     <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
-        
+
     //   </div>
     //   <dev>
     //     <a></a>{" "}
