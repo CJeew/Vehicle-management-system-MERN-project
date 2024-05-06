2f5ed808987097d682cc8dbb9d6e5906e7e3e7e7
Your Name <you@example.com>
2024-05-06 10:07:22 +0530

    Shajeeh marge

--- FRONTEND/src/Component/Financial/AddTransaction.js
+++ FRONTEND/src/Component/Financial/AddTransaction.js
@@ -80,14 +80,18 @@ export default function AddTransaction() {
         </div>
         <div className="flex flex-col">
           <label htmlFor="paymentType" className="text-m font-medium leading-6 text-gray-900 mb-1">PaymentType</label>
-          <input
+          <select
             type="text"
             name="paymentType"
             id="paymentType"
             value={paymentType}
             onChange={(e) => setPaymentType(e.target.value)}
             className="block rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm py-2 px-3"
-          />
+          >
+          <option value="Cash">Cash</option>
+          <option value="Cradit">Cradit Card</option>
+          <option value="Debit">Debit Card</option>
+        </select> 
         </div>
         <div className="flex flex-col">
           <label htmlFor="amount" className="text-m font-medium leading-6 text-gray-900 mb-1">Amount</label>
@@ -102,25 +106,34 @@ export default function AddTransaction() {
         </div>
         <div className="flex flex-col">
           <label htmlFor="accounts" className="text-m font-medium leading-6 text-gray-900 mb-1">Category</label>
-          <input
+          <select
             type="text"
             name="accounts"
             id="accounts"
             value={accounts}
             onChange={(e) => setAccounts(e.target.value)}
             className="block rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm py-2 px-3"
-          />
+          >
+            <option value="Income">Income</option>
+            <option value="Expanses">Expanses</option>
+            <option value="Tax">Tax</option>
+          </select>
         </div>
         <div className="flex flex-col">
           <label htmlFor="department" className="text-m font-medium leading-6 text-gray-900 mb-1">Department</label>
-          <input
+          <select
             type="text"
             name="department"
             id="department"
             value={department}
             onChange={(e) => setDepartment(e.target.value)}
             className="block rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm py-2 px-3"
-          />
+          >
+            <option value="Finance">Finance</option>
+            <option value="Inventory">Inventory</option>
+            <option value="Service">Service</option>
+            <option value="Booking">Booking</option>            
+          </select>
         </div>
       </div>
       <div className="mt-6 flex items-center justify-end gap-x-6">
@@ -132,8 +145,9 @@ export default function AddTransaction() {
         </button>
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
+          className="bg-gradient-to-r from-yellow-700 via-yellow-800 to-yellow-900 hover:from-amber-900 hover:via-amber-800 
+          hover:to-amber-700 text-white font-bold py-2 px-3 rounded-lg mr-2 opacity-90 transition duration-300 
+          ease-in-out transform hover:scale-105">
           Add
         </button>
       </div>
